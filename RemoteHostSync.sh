80b598f2c7050b45d54d27d986163bc3c92e5868
Jokel64 <jakob.englert@freenet.de>
2019-12-09 10:05:50 +0100

    Added webstreaming_2.py to deprecated

--- RemoteHostSync.sh
+++ RemoteHostSync.sh
@@ -1,2 +1,2 @@
 #rsync -delete -av -h -r -update /parcelcopter pi@10.42.0.153:/repo/parcelcopter.local
-rsync -r -a -v -e ssh --delete ../parcelcopter pi@10.42.0.153:/home/pi/repo/remote
+rsync -r -a -v -e ssh --delete ../parcelcopter pi@10.42.0.86:/home/pi/repo/remote
